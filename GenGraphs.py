ddf0b68725527bc21ab70c991fb3f6d66bd6ebb7
CJ-de-V <CorneldeVilliers01@gmail.com>
2023-07-14 14:25:09 +0200

    Ran a batch of experiments, 100k equilibration and 100k data gathering very modest but sufficient for basic tests
    Dataextraction is now up and running, further testing on its correctness
    is necessary.
    
    basic version of gengraphs is in the works for visualizing what we're even doing

---
+++ GenGraphs.py
@@ -0,0 +1,71 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import scale as scl
+import pandas as pd
+import sys
+from matplotlib.backends.backend_pdf import PdfPages
+from itertools import cycle
+
+# makes a cycle that lets us iterate over which colors to use
+from distinctipy import distinctipy
+
+colors = distinctipy.get_colors(10)
+print(colors)
+number = 1
+
+
+# Pandas dataframe, x dependent & y independent variable, variable on legend and number of figure for plotting
+# figure numbers have to be distinct
+def loglogplot(datfram, Xdata, Ydata, legend, xerror=0, yerror=0):
+    global number
+    plt.figure(number)
+    number += 1
+    legset = datfram[legend].unique()
+
+    numcolz = len(legset)  # number of different colors
+    for i in range(len(legset)):
+        ndata = df.loc[df[legend] == legset[i]]
+        x = np.array(ndata[Xdata].tolist())
+        y = np.array(ndata[Ydata].tolist())
+        plt.loglog(x, y, 'o-', base=2, c=colors[i])
+
+        if xerror != 0:
+            plt.errorbar(x, y, xerr=np.array(ndata[xerror].tolist()), c=colors[i])
+        if yerror != 0:
+            plt.errorbar(x, y, yerr=np.array(ndata[yerror].tolist()), c=colors[i])
+
+    plt.title("Log-Log plot of " + Xdata + " vs " + Ydata + " for various " + legend)
+    plt.xlabel("Log(" + Xdata + ")")
+    plt.ylabel("Log(" + Ydata + ")")
+    plt.legend(legset, title=legend)
+
+
+def save_multi_image(filename):
+    pp = PdfPages(filename)
+    fig_nums = plt.get_fignums()
+    figs = [plt.figure(n) for n in fig_nums]
+    for fig in figs:
+        fig.savefig(pp, format='pdf')
+    pp.close()
+
+
+df = pd.read_csv('finaldata.csv')
+
+# plot ln Lp vs ln k
+# print(df)
+loglogplot(df, 'Kp', 'lp', 'Np', yerror='sd_lp')
+# plot ln N vs ln Rg^2
+
+loglogplot(df, 'Np', 'Rg', 'Kp', yerror='sd_Rg')
+
+# plot ln R_g vs ln lp
+
+#loglogplot(df, 'R_g', 'lp', 'k', xerror='R_g_sd', yerror='lp_sd')
+
+# plot ln R_g vs ln lp
+
+#loglogplot(df, 'R_g', 'lp', 'N', xerror='lp_sd', yerror='R_g_sd')
+
+# save_multi_image("FinalGraphs/Results.pdf")
+
+plt.show()
--- dataextraction.py
+++ dataextraction.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 
-avgbondlength = 0
-
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -27,49 +25,49 @@ def test(x, b):
 
 
 np.set_printoptions(threshold=np.inf)
+basedir = os.getcwd()
+df = pd.DataFrame(columns=["Np", "Nt", "Kp", "Kt", "lp", "Rg", "zd", "lb", "sd_lp", "sd_Rg", "sd_zd"])
 p1 = np.linspace(0., 0., 3)
 p2 = np.linspace(0., 0., 3)
 N = 0  # Number of particles
 Nb = 0  # number of bonds
 Nc = 0  # number of correlation functions
 bondvectors = 0
+avgbondlength = 0
+Np = [8 , 32, 64, 128 , 256]  # [8 ,16, 32, 64, 128] [0.25,  1, 4, 16, 64]
+Nt = [8 , 32, 64,128 , 256]
+Kp = [0.25,  1, 4, 16, 64]
+Kt = [0.25,  1, 4, 16, 64]
 
-Np = [8, 16, 32] # excluded the 64 and beyond ones for now since we simply wish to test our creation
-Nt = [8, 16, 32]
-Kp = [0.25,  1, 4, 16]
-Kt = [0.25, 1, 4, 16]
-basedir = os.getcwd()
-pd.dataframe("Np", "Nt", "Kp", "Kt", "lp", "Rg", "zd", "lb", "sd_lp", "sd_Rg", "sd_zd")
-for np in Np:
+for nop in Np:
     for nt in Nt:
         for kp in Kp:
             for kt in Kt:
-                discriminator ="Np_" + str(np) + "_Nt_" + str(nt) + "_Kp_" + str(kp) + "_Kt_" + str(
-                    kt)
-                os.chdir(basedir+"/output/"+discriminator)
+                discriminator = "Np_" + str(nop) + "_Nt_" + str(nt) + "_Kp_" + str(kp) + "_Kt_" + str(kt)
+                os.chdir(basedir + "/output/" + discriminator)
+                Nb = nop - 1
+                Nc = Nb - 1
+                bondvectors = np.zeros((Nb, 3))
+                correlationfunctions = np.zeros(Nc)
+                avgbondlength = 0
+
+                numavg = 0  # Number of timesteps we're averaging over, used for the normalization
                 print('starting with analysis of: ' + discriminator)
-                with open('dump'+discriminator + '.dynamics') as datafile:
-                # continue from here in modernization
-                    avgbondlength = 0
-                    skiplines(datafile, 3)
-                    N = int(datafile.readline())
-                    Nb = N - 1
-                    skiplines(datafile, 5)
-                    bondvectors = np.zeros((Nb, 3))
 
-                    Nc = Nb - 1
-                    correlationfunctions = np.zeros(Nc)
+                # persistence and bond length calculation
+                with open('dump' + discriminator + '.dynamics') as datafile:
                     # Index-1 of the above indicates how many atoms separate the bondvectors
-
+                    skiplines(datafile, 9)  # skips first boilerplate
                     line = 'liney'
-                    numavg = 0  # Number of timesteps we're averaging over, used for the normalization
+
                     while line != '':
                         line = (datafile.readline()).split(" ")
                         p1 = np.array([float(line[2]), float(line[3]), float(line[4])])
                         # position 1 1 is now real
                         p2 = np.array([0.0, 0.0, 0.0])
                         # reads particle 2-N in and finds the bondvectors
-                        for i in range(1, N):
+
+                        for i in range(1, nop):  # reading past entry 1 which we already read
                             line = (datafile.readline()).split(" ")
                             p2 = np.array([float(line[2]), float(line[3]), float(line[4])])
                             bondvectors[i - 1] = normalize(np.subtract(p2, p1))
@@ -80,8 +78,10 @@ for np in Np:
                             for j in range(0, Nb - i):  # iterates over all legal bonds with i bonds between them
                                 runninavg += np.dot(bondvectors[j], bondvectors[j + i])  # Here be where we absed
                             correlationfunctions[i] += runninavg / (Nb - i)
-                        skiplines(datafile, 8)
-                        line = datafile.readline()
+
+                        skiplines(datafile, nt)  # skips the tether's lines
+                        skiplines(datafile, 8)  # skips boilerplate
+                        line = datafile.readline()  # reads in last line of boilerplate to confirm we're not at EOF
                         numavg += 1
                         # finished reading in and processing one timestep
 
@@ -92,25 +92,37 @@ for np in Np:
                     weights = np.reciprocal(np.arange(Nc + 0.0, 0.0, -1.0))
                     param, param_cov = curve_fit(test, x, y, maxfev=1000, sigma=weights)
                     lp = param[0]
-                    # R_gs hootenanny
+                    sd_lp = np.sqrt(param_cov[0][0])
 
-                    with open('N' + str(nmonom) + '/radius_of_gyration_squared_N' + str(nmonom) + '_k' + str(
-                            kval) + '.dat') as datafile:
-                        # readandmakebusiness
-                        fulldat = datafile.readlines()
-                        fulldat.pop(0)
-                        for i in range(len(fulldat)):
-                            fulldat[i] = float(fulldat[i].split(' ')[1])
-                        Rg = numpy.mean(fulldat)
-                        RgVar = numpy.var(fulldat)
+                # RG data
+                with open('radius_of_gyration' + discriminator + '.dat') as datafile:
+                    fulldat = datafile.readlines()  # these files are short enough for us to just use readlines
+                    fulldat.pop(0)
+                    for i in range(len(fulldat)):
+                        fulldat[i] = float(fulldat[i].split(' ')[1])
+                    Rg = np.mean(fulldat)
+                    sd_Rg = np.var(fulldat)
 
-                    datfile.write(str(nmonom) + ', ')  # N
-                    datfile.write(str(kval) + ', ')  # k
-                    datfile.write(str(lp) + ', ')  # lp
-                    # below is from switching to LJ units from bondvector units
-                    datfile.write(str(np.sqrt(param_cov[0][0])) + ', ')  # SD of lp
+                # Zdistance data
 
-                    datfile.write(str(Rg) + ', ')  # Rg
-                    datfile.write(str(np.sqrt(RgVar)) + ', ')  # SD of Rg
-                    datfile.write(str(avgbondlength) + '\n')  # lb
-# datfile.close()
+                with open('dump' + discriminator + '.dynamics') as datafile:
+                    skiplines(datafile, 7)
+                    zpeak = float(datafile.readline().split(" ")[1]) - 1  # finds walls Z pos
+
+                with open('pcom' + discriminator + '.dat') as datafile:
+                    fulldat = datafile.readlines()[2:]  # pops 2 first lines
+                    for i in range(len(fulldat)):
+                        fulldat[i] = float(fulldat[i].split(' ')[3])
+                    numzdata = np.array(fulldat)
+                    numzdata=zpeak-numzdata
+                    zdis = np.mean(numzdata)
+                    sd_zd = np.sqrt(np.var(numzdata))
+
+                entry = [nop, nt, kp, kt, lp, Rg, zdis,
+                         avgbondlength, sd_lp, sd_Rg, sd_zd]
+                datline = np.asarray(entry, dtype=float)
+                df.loc[len(df)] = datline
+print(df, '\n')
+os.chdir(basedir)
+df.to_csv('finaldata.csv')
+# print you pandas here
